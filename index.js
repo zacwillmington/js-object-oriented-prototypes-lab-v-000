434707760d7cc7839e689046ff8f7010b97bc2e2
Zac Willmington <zac.willmington@gmail.com>
2018-09-10 20:28:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ function BoardMember(name, homeState, training){
   this.homeState = homeState
   this.training = training
 }
-debugger;
-  User.prototype.veto = function(){
+
+  BoardMember.prototype.veto = function(){
     return 'No, I must disagree'
     };
-  // User.prototype.approve = function(){
-  //   return 'You can do that!'
-  //   };
-  // User.prototype.doCharity = function(){
-  //   return "I like to help people."
-  //   };
-  //
-  // User.prototype.releasePressStatement = function(){
-  //   return "You will see great things from Scuber."
-  //   };
-  //
-  // User.prototype.sayHi = function(){
-  //   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-  //   };
+  BoardMember.prototype.approve = function(){
+    return 'You can do that!'
+    };
+  BoardMember.prototype.doCharity = function(){
+    return "I like to help people."
+    };
+  
+  BoardMember.prototype.releasePressStatement = function(){
+    return "You will see great things from Scuber."
+    };
+  
+  BoardMember.prototype.sayHi = function(){
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+    };
