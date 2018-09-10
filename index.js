d892f41d7c8a6dc342976d016512953cebac36d2
Zac Willmington <zac.willmington@gmail.com>
2018-09-10 20:24:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,22 +2,21 @@ function BoardMember(name, homeState, training){
   this.name = name
   this.homeState = homeState
   this.training = training
-//
-//   this.veto = function(){
-//     return 'No, I must disagree'
-//   }
-//   this.approve = function(){
-//     return 'You can do that!'
-//   }
-//   this.doCharity = function(){
-//     return "I like to help people."
-//   }
-//
-//   this.releasePressStatement = function(){
-//     return "You will see great things from Scuber."
-//   }
-//
-//   this.sayHi = function(){
-//     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-//   }
 }
+  User.prototype.veto = function(){
+    return 'No, I must disagree'
+  }
+  User.prototype.approve = function(){
+    return 'You can do that!'
+  }
+  User.prototype.doCharity = function(){
+    return "I like to help people."
+  }
+
+  User.prototype.releasePressStatement = function(){
+    return "You will see great things from Scuber."
+  }
+
+  User.prototype.sayHi = function(){
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  }
