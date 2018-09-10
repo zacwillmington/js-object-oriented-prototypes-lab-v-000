abe645b1f2387552ad834354609e48d7d990e00b
Zac Willmington <zac.willmington@gmail.com>
2018-09-10 20:26:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ function BoardMember(name, homeState, training){
   this.homeState = homeState
   this.training = training
 }
-  User.prototype.veto = function(){
-    return 'No, I must disagree'
-    };
-  User.prototype.approve = function(){
-    return 'You can do that!'
-    };
-  User.prototype.doCharity = function(){
-    return "I like to help people."
-    };
-
-  User.prototype.releasePressStatement = function(){
-    return "You will see great things from Scuber."
-    };
-
-  User.prototype.sayHi = function(){
-    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-    };
+  // User.prototype.veto = function(){
+  //   return 'No, I must disagree'
+  //   };
+  // User.prototype.approve = function(){
+  //   return 'You can do that!'
+  //   };
+  // User.prototype.doCharity = function(){
+  //   return "I like to help people."
+  //   };
+  // 
+  // User.prototype.releasePressStatement = function(){
+  //   return "You will see great things from Scuber."
+  //   };
+  // 
+  // User.prototype.sayHi = function(){
+  //   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  //   };
