2ca19088ef5ff44c11397928ae93f44f965b8bf5
Zac Willmington <zac.willmington@gmail.com>
2018-09-10 20:27:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function BoardMember(name, homeState, training){
   this.homeState = homeState
   this.training = training
 }
+debugger;
   User.prototype.veto = function(){
     return 'No, I must disagree'
     };
